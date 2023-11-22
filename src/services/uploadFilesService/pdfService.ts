b3ca5d1c92edfe7e1d13f5836177d0f2cb52a181
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-11-22 13:10:19 -0300

    feat: implements logic to search context on vector db

--- src/services/uploadFilesService/pdfService.ts
+++ src/services/uploadFilesService/pdfService.ts
@@ -33,8 +33,42 @@ export const save = async (embedDocs: Document<Record<string, any>>[]) => {
   }
 };
 
+export const search = async (question: string) => {
+  try {
+    const vectorStore = new PrismaVectorStore(new OpenAIEmbeddings(), {
+      db: prisma,
+      prisma: Prisma,
+      tableName: "Document",
+      vectorColumnName: "vector",
+      columns: {
+        id: PrismaVectorStore.IdColumn,
+        content: PrismaVectorStore.ContentColumn,
+      },
+    });
+
+    const documents = await vectorStore.similaritySearch(question, 10);
+
+    const context = documents.filter((document) => {
+      if (document.metadata?._distance) {
+        return (
+          Number(
+            document.metadata._distance
+              .toString()
+              .match(/^-?\d+(\.\d{1,2})?/)?.[0] || 0
+          ) <= 0.24
+        );
+      }
+    });
+
+    return context;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 const pdfService = {
   save,
+  search,
 };
 
 export default pdfService;
