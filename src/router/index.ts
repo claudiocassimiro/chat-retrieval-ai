d5f8d1c35e0330c9af0f9adb8d06b3fe7e9b0af6
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-11-23 08:20:08 -0300

    fix: hotfix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import multer from "multer";
-import pdfController from "../controllers/uploadFilesController/pdfController";
+import pdfController from "../controllers/pdfController/handlerPdf";
+import questionController from "../controllers/questionController/handlerQuestion";
 import { storage } from "../utils/multer/storage";
 
 const router = Router();
 
 const upload = multer({ storage: storage() });
 
-router.get("/", (_req, _res) => console.log("Hello, world!"));
 router.post("/api/files", upload.single("file"), pdfController.save);
-router.post("/api/question", pdfController.question);
+router.post("/api/question", questionController.question);
 
 export default router;
