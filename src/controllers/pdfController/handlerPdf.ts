fea058c43f8012281a570b7db0e8558e64d7c9f5
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-11-23 08:39:46 -0300

    refactor: remove logic to another file

--- src/controllers/pdfController/handlerPdf.ts
+++ src/controllers/pdfController/handlerPdf.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { splitTextsInChunks } from "../../utils/pdfs/splitTextsInChunks";
-import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { splitTextsIntoChunks } from "../../utils/pdfs/splitTextsIntoChunks";
+import { findPDFByFilename } from "../../utils/pdfs/findPDFByFilename";
 import pdfService from "../../services/uploadFilesService/pdfService";
 
 const save = async (req: Request, res: Response) => {
@@ -8,14 +8,9 @@ const save = async (req: Request, res: Response) => {
     return res.status(400).json({ message: "No file uploaded" });
   }
 
-  const loader = new PDFLoader(
-    // eslint-disable-next-line prettier/prettier
-    `src/document_loaders/pdfs/${req.file.filename}`
-  );
+  const docs = await findPDFByFilename(req.file.filename);
 
-  const docs = await loader.load();
-
-  const splitedDocument = await splitTextsInChunks(docs);
+  const splitedDocument = await splitTextsIntoChunks(docs);
 
   if (!splitedDocument) {
     return res.status(400).json({ message: "The uploaded file was empty" });
